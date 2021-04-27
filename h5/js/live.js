4e2d40d1bf5ec0cdb4ac1d717df13c199b79060d
yishengsuoaii <1609558084@qq.com>
2021-04-27 17:25:19 +0800

    添加小提示

--- h5/js/live.js
+++ h5/js/live.js
@@ -1,16 +1,16 @@
 let data = ''
-var infoData = [1,2]
-let event_uri_key = 'UlzoiFB2Vt6bHJX'
-// if(sessionStorage.getItem('cubeInfo')){
-//    data = sessionStorage.getItem('cubeInfo').substring(1).split("&")
-//     data.forEach(item => {
-//         infoData.push(item.split("="))
-//     })
-//     event_uri_key = window.location.search.substring(1).split("=")[1]
+var infoData = []
+let event_uri_key = ''
+if(sessionStorage.getItem('cubeInfo')){
+   data = sessionStorage.getItem('cubeInfo').substring(1).split("&")
+    data.forEach(item => {
+        infoData.push(item.split("="))
+    })
+    event_uri_key = window.location.search.substring(1).split("=")[1]
    
-// }else {
-//     window.location.href='http://www.cube.vip/h5/wxlogin.html?key=' + window.location.search.substring(1).split("=")[1]
-// }
+}else {
+    window.location.href='http://www.cube.vip/h5/wxlogin.html?key=' + window.location.search.substring(1).split("=")[1]
+}
 let timers = null
 let videoJs = ''
 let playFlay = false
